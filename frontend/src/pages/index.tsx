34d12eda49fa257df42d78ce0428048ae27e7901
Harsh Rana <hr23232323@gmail.com>
2021-12-29 00:44:15 -0500

    removed the coming soon banner

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,39 +1,66 @@
 import React from "react";
 import type { NextPage } from "next";
-import Typography from "@mui/material/Typography";
-import { Grid, Button } from "@mui/material";
-import { ArrowCircleRight } from "@mui/icons-material";
+import { Grid, Button, Typography } from "@mui/material";
+import AppContext from "../AppContext";
+import { DebtToEarningsData } from "../types";
+import { Navbar } from "../components/Navbar";
+import { SearchBar } from "../components/SearchBar";
+import { FilterDrawer } from "../components/FilterDrawer";
+import { SignupModal } from "../components/SignupModal";
+import { DataTable } from "../components/DataTable";
 
 const Home: NextPage = () => {
+  const {
+    isFilterDrawerOpen,
+    toggleFilterDrawer,
+    isUserSignupModalOpen,
+    closeUserSignupModal,
+    updateUserData,
+    rawData,
+  } = React.useContext(AppContext);
+
+  if (!rawData) {
+    // Deal with this later
+    return <></>;
+  }
+
   return (
     <Grid
       container
-      spacing={0}
       direction="column"
-      alignItems="center"
-      justifyContent="center"
+      spacing={0}
       style={{ minHeight: "100vh" }}
     >
-      <Grid
-        item
-        container
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-        xs={3}
-      >
-        <Typography variant="h2" gutterBottom>
-          Coming soon!
-        </Typography>
-        <Button
-          variant="contained"
-          color="success"
-          endIcon={<ArrowCircleRight />}
-          href="/dashboard"
-        >
-          Take a sneak peak
-        </Button>
+      <Grid item xs={2}>
+        <Navbar />
       </Grid>
+      <Grid item container direction="row" xs={2}>
+        <SearchBar />
+      </Grid>
+      <Grid item container direction="row" style={{ height: "80vh" }}>
+        <Grid item xs={1} />
+        <Grid
+          item
+          container
+          xs={10}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          style={{ height: "100%", width: "100%" }}
+        >
+          <Grid item xs={1} />
+          <Grid item container xs={11} style={{ width: "100%" }}>
+            <DataTable data={rawData} />
+          </Grid>
+        </Grid>
+        <Grid item xs={1} />
+      </Grid>
+      <FilterDrawer />
+      <SignupModal
+        isOpen={isUserSignupModalOpen}
+        onClose={closeUserSignupModal}
+        onSubmit={updateUserData}
+      />
     </Grid>
   );
 };
